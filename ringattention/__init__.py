00636428e08e67046e8d4c1dc2b2cc57301d1632
Wilson Yan <wilson1.yan@berkeley.edu>
2024-08-07 16:41:25 +0000

    ring attention inference

--- ringattention/__init__.py
+++ ringattention/__init__.py
@@ -3,6 +3,7 @@ It features blockwise computation for feedforward networks to reduce memory cost
 For more details, refer to RingAttention ('Ring Attention with Blockwise Transformers for Near-Infinite Context') at https://arxiv.org/abs/2310.01889 and BlockwiseTransformer ('Blockwise Parallel Transformer for Large Context Models') at https://arxiv.org/abs/2305.19370.
 """
 
+from .ringattention_inference import ring_attention_inference
 from .ringattention_jax import ring_attention, blockwise_feedforward
 from .ringattention_pallas_gpu import ring_flash_attention_gpu
 from .ringattention_pallas_tpu import ring_flash_attention_tpu
@@ -20,5 +21,7 @@ else:
     ringattention = ring_attention
 
 ringattention_jax = ring_attention
+ringattention_inference = ring_attention_inference
 
-__all__ = ["ringattention", "blockwise_feedforward", "ringattention_jax"]
+__all__ = ["ringattention", "blockwise_feedforward", "ringattention_jax",
+           "ringattention_inference"]
